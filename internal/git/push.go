ad5778801a4fb1d2ce3389fcd996348a5703935b
GitLab CI <gitlab-ci@callsign.com>
2018-06-22 10:09:48 +0100

    Initial import

--- internal/git/push.go
+++ internal/git/push.go
@@ -9,6 +9,12 @@ import (
 // Push the GitOps project changes
 func Push(projectName, commitMessage string) error {
 	fmt.Println("Pushing GitOps project changes")
+	if _, err := Git(".", "config", "--global", "user.name", "GitLab CI"); err != nil {
+		return err
+	}
+	if _, err := Git(".", "config", "--global", "user.email", "gitlab-ci@callsign.com"); err != nil {
+		return err
+	}
 	if _, err := Git(projectName, "add", "."); err != nil {
 		return err
 	}
